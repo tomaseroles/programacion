0c8a26743d2332586d9cfdc7f04e071a53159648
tomfor <tomfor@a1420>
2018-02-19 17:59:28 +0100

    practica 12. inicio

---
+++ tema6/src/practica12/p12.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica12;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tomfor
+ */
+public class p12 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        char sexo;
+        double peso;
+        int altura;
+        
+        System.out.print("Nombre: ");
+        nombre = sc.nextLine();
+        System.out.print("Sexo: ");
+        sexo = sc.nextLine().charAt(0);
+        System.out.print("Peso: ");
+        peso = sc.nextDouble();
+        System.out.print("Altura: ");
+        altura = sc.nextInt();
+        
+        persona[] v= new persona[3];
+        
+        v[0].setNombre(nombre);
+        v[0].setSexo(sexo);
+        v[0].setPeso(peso);
+        v[0].setAltura(altura);
+        
+        v[1].setNombre(nombre);
+        v[1].setSexo(sexo);
+        v[1].getPeso();
+        v[1].getAltura();
+        
+        v[2].getNombre();
+        v[2].getSexo();
+        v[2].getPeso();
+        v[2].getAltura();
+        
+    }
+}
---
+++ tema6/src/practica12/persona.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica12;
+
+/**
+ *
+ * @author tomfor
+ */
+public class persona {
+    String nombre;
+    int edad;
+    String DNI;
+    char sexo;
+    double peso;
+    int altura;
+    String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+    
+    void persona(){
+        nombre = "Nombre";
+        edad = 0;
+        sexo = 'H';
+        peso=0;
+        altura=0;
+    }
+    
+    void persona(String nombre, int edad, char sexo){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.sexo=sexo;
+    }
+    
+    void persona(String nombre, int edad, String dni, char sexo, double peso, int altura){
+        this.nombre=nombre;
+        this.edad = edad;
+        this.DNI=dni;
+        this.sexo=sexo;
+        this.peso=peso;
+        this.altura=altura;
+    }
+    
+    public double calcularIMC(){
+        double imc;
+        imc = getPeso()/getAltura()*getAltura();
+        if(imc<20)
+            return -1;
+        else if(imc>=20 && imc<=25)
+            return 0;
+        else
+            return 1;
+    }
+    
+    public String generaDNI(){
+        int ndni;
+        char letra;
+        
+        ndni = (int)Math.random()*100000000;
+        letra = letras.charAt(ndni/23);
+        
+        return Long.toString(ndni)+letra;
+    }
+    
+    public boolean mayorEdad(){
+        if (this.edad>=18)
+            return true;
+        else
+            return false;
+    }
+    
+    @Override public String toString(){
+        String salida;
+        salida = "Ficha completa:";
+        salida+= "Nombre:     " + getNombre();
+        salida+= "Edad:       " + getEdad();
+        salida+= "DNI:        " + getDNI();
+        salida+= "Sexo:       " + getSexo();
+        salida+= "Peso(kg):   " + getPeso();
+        salida+= "Altura(cm): " + getAltura();
+        return salida;
+    }
+    
+    public String getNombre(){ return this.nombre; }
+    
+    public void setNombre(String n){ this.nombre = n; }
+    
+    public int getEdad(){ return this.edad; }
+    
+    public void setEdad(int e){ this.edad=e; }
+    
+    public String getDNI(){ return DNI; }
+        
+    public char getSexo(){ return sexo; }
+    
+    public void setSexo(char s){ this.sexo = s; }
+    
+    public double getPeso(){ return peso; }
+    
+    public void setPeso(double p){ this.peso=p; }
+    
+    public int getAltura(){ return altura; }
+    
+    public void setAltura(int a){ this.altura=a; }
+            
+}
